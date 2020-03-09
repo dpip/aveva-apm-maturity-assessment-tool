fd178202d3c7fd09483289a5be797e633dcabad7
Daniel Pipkin <danielpipkin@Centerlines-MacBook-Pro.local>
2020-03-09 17:05:46 -0400

    fine tuning animation duration delay and styles // upgrades gatsby version to match fotfix

--- src/components/resultAnimation.js
+++ src/components/resultAnimation.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import CountUp from 'react-countup';
 
 const Chart = (props) => {
     const [result, setResult] = useState(0);
@@ -8,10 +9,9 @@ const Chart = (props) => {
         const timer = setTimeout(() => {
             setResult((props.result * 31.4) / 100)
             setStrokeResult((props.result * 61.23) / 100)
-        }, 1000);
+        }, 750);
         return () => clearTimeout(timer)
     })
-
 
     return (
         <div className="result-animation">
@@ -19,19 +19,19 @@ const Chart = (props) => {
                 <svg viewBox="0 0 20 20">
                     <defs>
                         <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
-                            <stop offset="0%" stop-color="#ffbc00" />
-                            <stop offset={`${strokeResult}`} stop-color="#f70656" />
+                            <stop offset="0%" stop-color="#f70656" />
+                            <stop offset={`${strokeResult}`} stop-color="#ffbc00" />
                         </linearGradient>
                     </defs>
-                    <circle r="10" cx="10" cy="10" fill="#6b04a8" />
+                    <circle r="10" cx="10" cy="10" fill="#3D1152" />
                     <circle r="9.75" cx="10" cy="10"
                         fill="transparent"
                         stroke="#fff"
-                        stroke-width="0.5"
+                        stroke-width="0.25"
                     />
                     <circle r="5" cx="10" cy="10"
                         fill="transparent"
-                        stroke="#3d1152"
+                        stroke="rgba(0, 0, 0, 0.4)"
                         stroke-width="10"
                         stroke-dasharray={`${result} 31.4`}
                         transform="rotate(-90)"
@@ -39,14 +39,14 @@ const Chart = (props) => {
                     <circle r="9.75" cx="10" cy="10"
                         fill="transparent"
                         stroke="url(#gradient)"
-                        stroke-width="0.5"
+                        stroke-width="0.25"
                         stroke-dasharray={`${strokeResult} 61.23`}
                         transform="rotate(-90)"
                     />
                 </svg>
                 <div className="result-content">
-                    <p>Your APM maturity assessment score is</p>
-                    <p>{props.result}%</p>
+                    <p><CountUp end={props.result} delay={1.5} duration={1.25} />%</p>
+                    <p>is your score</p>
                 </div>
             </div>
             <div className="bottom-container">
@@ -54,36 +54,36 @@ const Chart = (props) => {
                     className="over"
                     style={{ textAlign: `left` }}
                 >
-                    <li style={{ color: props.bracket === 3 ? "#ffbc00" : "#fff" }}>
+                    <li style={{ color: props.bracket === 3 ? "#ffbc00" : "#BECCD6" }}>
                         <div className="dots">
-                            <div />
-                            <div />
-                            <div />
-                            <div />
+                            <div style={{ backgroundColor: props.bracket === 3 ? "#ffbc00" : "#BECCD6" }} />
+                            <div style={{ backgroundColor: props.bracket === 3 ? "#ffbc00" : "#BECCD6" }} />
+                            <div style={{ backgroundColor: props.bracket === 3 ? "#ffbc00" : "#BECCD6" }} />
+                            <div style={{ backgroundColor: props.bracket === 3 ? "#ffbc00" : "#BECCD6" }} />
                         </div>
                         Raise the industry bar
-          </li>
-                    <li style={{ color: props.bracket === 2 ? "#ffbc00" : "#fff" }}>
+                        </li>
+                    <li style={{ color: props.bracket === 2 ? "#ffbc00" : "#BECCD6" }}>
                         <div className="dots">
-                            <div />
-                            <div />
-                            <div />
+                            <div style={{ backgroundColor: props.bracket === 2 ? "#ffbc00" : "#BECCD6" }} />
+                            <div style={{ backgroundColor: props.bracket === 2 ? "#ffbc00" : "#BECCD6" }} />
+                            <div style={{ backgroundColor: props.bracket === 2 ? "#ffbc00" : "#BECCD6" }} />
                         </div>
                         Path to organizational excellence
-          </li>
-                    <li style={{ color: props.bracket === 1 ? "#ffbc00" : "#fff" }}>
+                        </li>
+                    <li style={{ color: props.bracket === 1 ? "#ffbc00" : "#BECCD6" }}>
                         <div className="dots">
-                            <div />
-                            <div />
+                            <div style={{ backgroundColor: props.bracket === 1 ? "#ffbc00" : "#BECCD6" }} />
+                            <div style={{ backgroundColor: props.bracket === 1 ? "#ffbc00" : "#BECCD6" }} />
                         </div>
                         Close your performance gaps
-          </li>
-                    <li style={{ color: props.bracket === 0 ? "#ffbc00" : "#fff" }}>
+                        </li>
+                    <li style={{ color: props.bracket === 0 ? "#ffbc00" : "#BECCD6" }}>
                         <div className="dots">
-                            <div />
+                            <div style={{ backgroundColor: props.bracket === 0 ? "#ffbc00" : "#BECCD6" }} />
                         </div>
                         Take APM to the next level
-          </li>
+                        </li>
                 </ul>
                 <h2 className="over"
                     style={{ fontSize: `1.25em`, lineHeight: 1.2, textAlign: `center`, padding: `1em 0.5em 0` }}>Learn what you can do to improve your results.</h2>
